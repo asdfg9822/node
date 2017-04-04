ca4df745bbe909866a9469b364a69d6697a3b735
JongHyeok Choi <asdfg9822@naver.com>
2017-04-04 19:31:27 +0900

    File System Module Test

--- fs_recursive_functional.js
+++ fs_recursive_functional.js
@@ -3,7 +3,14 @@
  */
 
 var fs = require('fs');
+var vm = require('vm');
 var _ = require('underscore')
+
+var UTIL = {
+    emptyString: function() {
+        return '';
+    }
+};
 
 function getBaseTmp() {
 
@@ -44,13 +51,13 @@ Object to Source Code
 */
 function Obj2Source(obj) {
     if(!_.isObject(obj)) {
-        console.error(obj);
-        console.error("Above Data Type is not Object");
+        //console.error(obj);
+      //  console.error("Above Data Type is not Object");
         return;
     }
 
     var source = JSON.stringify(obj);
-    console.log(source);
+    //console.log(source);
 
     source.replace('')
 
@@ -62,7 +69,7 @@ function objToString (obj, defaultString) {
 
         var value = !_.isFunction(curr) && _.isObject(curr) ? objToString(curr, source) : curr;
 
-        console.log(key + ":" + value);
+     //   console.log(key + ":" + value);
         return source;
 
     }, defaultString);
@@ -73,7 +80,7 @@ var result = objToString({1:'2', 2:function() {
     return this;
 }, obj: { obj2: {}}});
 
-console.log(result)
+//console.log(result)
 
 
 
@@ -84,3 +91,32 @@ function addClass(target, className) {
 
 }
 
+/*
+String
+*/
+function removeTransformGenerator() {
+    var startRegex = /^#\{(.*)\}$/;
+    return function(str) {
+        if(typeof str === 'string') {
+            return str.replace(startRegex, '$1');
+        }
+        return str;
+    }
+}
+
+var rmTransformChar = removeTransformGenerator();
+var result = rmTransformChar('#{okok}');
+//console.log(result);
+
+
+/*
+ EXT JS
+ */
+global.Ext = {
+    define: function (id, props) {
+        console.log(id);
+        console.log(props);
+    }
+};
+
+require('./test/testfile.js');
